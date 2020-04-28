5ea5cfb7bc42453bfb5c5ddcd55c0281ed54e02e
Eric Lombardo <ejlanim8or@gmail.com>
2020-04-28 14:35:06 -0400

    intial setup of project

---
+++ State.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 20)] [SerializeField] string storyText;
+}
